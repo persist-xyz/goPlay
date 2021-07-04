af95f81e3d7a3c4d54ddeb1578c3013c5400e24f
朱晓云 <zhuxiaoyun@dushu.io>
2021-07-04 21:16:46 +0800

    init

--- src/app.config.js
+++ src/app.config.js
@@ -1,5 +1,28 @@
 export default {
-  pages: ["pages/index/index", "pages/webview/webview"],
+  pages: [
+    "pages/index/index",
+    "pages/my/my",
+    "pages/webview/webview"
+  ],
+  tabBar: {
+    color: "#000000",
+    selectedColor: "#000000",
+    backgroundColor: "#fff",
+    list: [
+      {
+      pagePath: "pages/index/index",
+      text: "首页"
+    },
+      {
+        pagePath: "pages/my/my",
+        text: "发布"
+      },
+      {
+        pagePath: "pages/my/my",
+        text: "我的"
+      },
+    ]
+    },
   window: {
     backgroundTextStyle: "light",
     navigationBarBackgroundColor: "#fff",
---
+++ src/pages/my/my.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { View, Text } from "@tarojs/components";
+
+import "./my.scss";
+
+const My = () => {
+  return (
+    <View className="my">
+      <Text>my</Text>
+    </View>
+  );
+};
+
+export default My;
